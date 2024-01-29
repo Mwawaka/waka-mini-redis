9cfdc36a6aa72d8e105e8e7edeb82f065e31628a
Mwawaka <kedskeds90@gmail.com>
2024-01-29 11:15:06 +0300

    update: read and write to buffer

--- main.go
+++ main.go
@@ -1,29 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 )
 
+const (
+	addressString = "0.0.0.0:6379"
+)
+
 func main() {
 
-	listener, err := net.Listen("tcp", "0.0.0.0:6370")
-
+	err := run()
 	if err != nil {
-		log.Fatal("Failed to bind to port 6379", err)
+		log.Fatal(err)
 	}
 
-	defer func(lis net.Listener) {
-		err := lis.Close()
+}
+
+func run() error {
+	listener, err := net.Listen("tcp", addressString)
+
+	if err != nil {
+		return fmt.Errorf("failed to bind to port adress: %s : %v\n", addressString, err)
+	}
+
+	defer func(l net.Listener) {
+		err := l.Close()
 		if err != nil {
-			log.Fatal("Failed to close: ", err)
+			log.Printf("failed to close listener: %v\n", err)
 		}
 	}(listener)
-
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Fatal("Error accepting connections: ", err)
+			return fmt.Errorf("error accepting connections: %v\n", err)
 		}
 		handleClient(conn)
 	}
@@ -34,7 +46,26 @@ func handleClient(conn net.Conn) {
 	defer func(con net.Conn) {
 		err := con.Close()
 		if err != nil {
-			log.Fatal("Failed to close connection: ", err)
+			log.Fatal("failed to close connection: ", err)
 		}
 	}(conn)
+
+	//Read Data
+	buf := make([]byte, 128)
+
+	n, err := conn.Read(buf)
+
+	if err != nil {
+		log.Println("error reading from buffer: ", err)
+		return
+	}
+	log.Printf("data read: %s\n", string(buf[:n]))
+
+	res := "+PONG\r\n"
+	_, err = conn.Write([]byte(res))
+
+	if err != nil {
+		log.Println("error writing to buffer: ", err)
+	}
+
 }
